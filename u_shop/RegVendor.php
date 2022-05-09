262fe386e7b988d10bdeb671d9d2aced58a9790e
ahmedmahmoud96 <82849059+ahmedmahmoud96@users.noreply.github.com>
2022-05-09 18:15:34 +0300

    Add files via upload

---
+++ u_shop/RegVendor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+تسجيل التاجر
+<form action="index.php" method="POST">
+
+
+  <input type="text" id="" name="name"><br>
+
+  <input type="email" id="" name="email"><br>
+
+  <input type="text" id="" name="phone"><br>
+
+  <input type="password" id="" name="password"><br>
+
+  <input type="submit" value="تسجيل الدخول " name="RegVen-form" class="BT btn-lg">
+  
+</form>
+</body>
+</html>
